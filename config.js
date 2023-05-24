653e6f5c8b293d8667b86a58199e6081530f5647
Gaëtan Chêne <gaetan.chene@hotmail.com>
2023-05-24 08:35:56 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    token: process.env.BOT_TOKEN,
-    DB_host: process.env.DB_HOST,
-    DB_user: process.env.DB_USER,
-    DB_pass: process.env.DB_PASS,
-    DB_name: process.env.DB_NAME,
-}
